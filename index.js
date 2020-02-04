b2f504340dc737780b11ab0b7806b31a34190f50
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2020-02-04 03:07:30 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+
+  const project = projects.find(p => p.id == id);
+  
+  if (!project) {
+    return res.status(400).json({ error: 'Project does not exists' });
+  }
+
+  return next();
+}
+
+server.use((req, res, next) => {
+  console.count("Contagem");
+
+  return next();
+});
+
+server.post('/projects', (req, res) => {
+  const { id } = req.body;
+  const { title } = req.body;
+
+  const project = { id, title, tasks: [] };
+  projects.push(project);
+  return res.json(project)
+});
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+  return res.json(projects);
+});
+
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+   projects.splice(projects.findIndex(p => p.id == id), 1);
+
+   return res.send();
+});
+
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  
+  const project = projects.find(p => p.id == id);
+  project.tasks.push(title);
+  
+  return res.json(project);
+});
+
+server.listen(3000);
